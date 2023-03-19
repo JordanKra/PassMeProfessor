8d9585211f5ab0abc2cf7d4e1e916d4e7c023ef7
Christian Cuevas <cmc23soccer@gmail.com>
2023-03-19 16:10:20 -0400

    Basic Timer Implemented

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.passmeprofessor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.view.View
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timerText = findViewById<TextView>(R.id.timerText)
+
+        object : CountDownTimer(65000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                var seconds = millisUntilFinished/1000/60
+                if(seconds == 0L){
+                    seconds = millisUntilFinished/1000
+                } else {
+                    seconds = millisUntilFinished / 60/ 1000
+                }
+                timerText.setText("" + millisUntilFinished/1000/60 + ":" + seconds)
+            }
+
+            override fun onFinish() {
+                timerText.setText("done!")
+            }
+        }.start()
+
     }
+
+
 }
