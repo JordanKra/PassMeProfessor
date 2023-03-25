b84934bbfe93496a5ecbd84b16262edbd4d16a24
Christian Cuevas <cmc23soccer@gmail.com>
2023-03-25 17:19:49 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -14,9 +14,6 @@ class MainActivity : AppCompatActivity() {
 
         val timerText = findViewById<TextView>(R.id.timerText)
 
-        val btn = findViewById<Button>(R.id.button)
-
-
         var timer =  Timer(95, timerText)
 
 
