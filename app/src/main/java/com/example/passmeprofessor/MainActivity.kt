a33d98c8f2a2b7c1400f8e5bcc5e14f8b1635cb1
Christian Cuevas <cmc23soccer@gmail.com>
2023-03-25 16:38:20 -0400

    Timer class implemented

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -13,22 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         val timerText = findViewById<TextView>(R.id.timerText)
 
-        object : CountDownTimer(65000, 1000) {
-
-            override fun onTick(millisUntilFinished: Long) {
-                var seconds = millisUntilFinished/1000/60
-                if(seconds == 0L){
-                    seconds = millisUntilFinished/1000
-                } else {
-                    seconds = millisUntilFinished / 60/ 1000
-                }
-                timerText.setText("" + millisUntilFinished/1000/60 + ":" + seconds)
-            }
-
-            override fun onFinish() {
-                timerText.setText("done!")
-            }
-        }.start()
+        var timer =  Timer(95, timerText)
 
     }
 
