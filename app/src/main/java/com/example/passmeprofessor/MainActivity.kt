99c0c40feea61e31b384379a3b2e0aa4375e59ee
Christian Cuevas <cmc23soccer@gmail.com>
2023-03-25 17:00:14 -0400

    Add time function in Timer class

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import android.view.View
 import android.os.CountDownTimer
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val timerText = findViewById<TextView>(R.id.timerText)
+
+        //val btn = findViewById<Button>(R.id.button)
+
 
         var timer =  Timer(95, timerText)
 
