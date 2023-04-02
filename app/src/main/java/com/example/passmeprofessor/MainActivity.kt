df65911bd7e50a4a8c30f2b8b7ad1c3799801dec
zcythan <jferrick@umich.edu>
2023-04-02 15:38:18 -0400

    Blur functions added

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.passmeprofessor
 
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.util.Log
 import android.view.GestureDetector
@@ -9,6 +11,8 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,35 @@ class MainActivity : AppCompatActivity() {
     private val RIGHT_SWIPE_THRESH: Float = 500.0f
 
     @SuppressLint("ClickableViewAccessibility")
+    private lateinit var originalBitmap: Bitmap
+    private fun captureViewBitmap(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(android.graphics.Color.WHITE)
+        }
+        view.draw(canvas)
+        return bitmap
+    }
+
+    private fun applyBlurToBackground() {
+        val rootView = findViewById<View>(android.R.id.content)
+        val blurredBackground = findViewById<ImageView>(R.id.blurred_background)
+
+        originalBitmap = captureViewBitmap(rootView)
+        val mainActivityBitmap = captureViewBitmap(rootView)
+        val blurredBitmap = BlurBuilder.blur(this, mainActivityBitmap)
+        blurredBackground.setImageBitmap(blurredBitmap)
+    }
+
+    private fun removeBlurFromBackground() {
+        val blurredBackground = findViewById<ImageView>(R.id.blurred_background)
+        blurredBackground.setImageBitmap(originalBitmap)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the screen to main screen
@@ -48,6 +81,26 @@ class MainActivity : AppCompatActivity() {
                 paperView.x = initialX + diffX
             }
         })
+        var blurred = true
+        val timerText = findViewById<TextView>(R.id.timerText)
+        //var timer =  Timer(95, timerText)
+
+        val blurButton = findViewById<Button>(R.id.blurButton)
+        blurButton.setOnClickListener {
+            blurred = if(blurred){
+                removeBlurFromBackground()
+                false
+            } else {
+                applyBlurToBackground()
+                true
+            }
+        }
+
+        val rootView = findViewById<View>(android.R.id.content)
+        rootView.post {
+            applyBlurToBackground()
+        }
+
 
         paperView.setOnTouchListener { _, motionEvent ->
             gestureDetector.onTouchEvent(motionEvent)
