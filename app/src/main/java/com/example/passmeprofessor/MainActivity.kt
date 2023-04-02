8740d42dbe577d532d7336620ff0e26193a1feca
zcythan <jferrick@umich.edu>
2023-04-02 15:51:50 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.CountDownTimer
