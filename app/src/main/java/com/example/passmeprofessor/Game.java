99e35065db1a08c708917fc8d6dee7a051ecd505
nicobokha <nebokha@umich.edu>
2023-04-09 01:26:08 -0400

    Remove unused methods. Creates and adds SwipeEventListeners Paper & Timer Jordan - implement string tags on paper_a, paper_b...in activity_main.xml John - implement showGameOver() Christian - implement Game.updateScore() and Timer.onSwipeEvent(event)

--- app/src/main/java/com/example/passmeprofessor/Game.java
+++ app/src/main/java/com/example/passmeprofessor/Game.java
@@ -23,10 +23,15 @@ public class Game {
         TimerEndListeners.add(listener);
     }
 
+    public void addSwipeEventListener(SwipeListener listener){
+        SwipeListeners.add(listener);
+    }
+
     //Performs all operations necessary to build first Timer
     public void buildTimer(int totalGameSeconds, TextView timerText) {
         timer = new Timer(totalGameSeconds, timerText, this);
         addTimerEndEventListener(this.timer);
+        addSwipeEventListener(this.timer);
     }
 
     //Performs all operations necessary to build first Rubric
@@ -37,47 +42,62 @@ public class Game {
     //Performs all operations necessary to build first Rubric
     public void buildPaper(ImageView imgView) {
         currentPaper = new Paper(imgView);
+        addSwipeEventListener(this.currentPaper);
     }
 
-    //Add a number of seconds to the timer
-    public void addTime(int addGameSeconds) {
-        timer.addTime(addGameSeconds);
+    //Update score based on whether the SwipeEvent was correct
+    public void updateScore(SwipeEvent event) {
+        //Christian
+        // please use method to encapsulate your logic for updating the score
+        // on the condition that the swipe is correct
+        //event has attribute Boolean correct that is initialized in Game.evalSwipe()
+
     }
 
-    public void subtractTime(int subtractGameSeconds){
-        timer.subtractTime(subtractGameSeconds);
-    }
+    public SwipeEvent evalSwipe(SwipeEvent event) {
 
-    //Add a number of points to the score
-    public void addScore(long addScorePoints) {
-        score += addScorePoints;
-    }
-
-    public void evalSwipe() {
-
+        //Get/Store the tag of the currentPaper's ImageView, use getTag, expect the values from R.string.letter_a, R.string.letter_b, .... see strings.xml
+        String paperLetterGrade = String.valueOf(currentPaper.getView().getTag());
+        //Get the Boolean PASS/FAIL value of the letter grade from the rubric
+        Boolean correctAnswer = currentRubric.getPassFailFromGrade(paperLetterGrade);
+        Boolean userGuess = event.swipeDirection;
+        if(userGuess == correctAnswer){
+            event.correct = true;
+        }
+        else{
+            event.correct = false;
+        }
+        return event;
     }
 
     public void showGameOver() {
-
+        //John
+        // please use this method to encapsulate all your game over screen state changes
     }
 
     public void setNewPaper() {
         currentPaper.generateRandomPaper();
     }
 
-    public void updateUI() {
-        // currentPaper.updateUI()
-        //
-    }
-
-    public static void sendTimerEndEvent(){
-
-    }
 
     public void fireTimerEndEvent(TimerEndEvent event){
         for(TimerEndListener listener: TimerEndListeners){
             listener.onTimerEnd(event);
         }
+        //implement game over screen here
+        //call methods that present game over screen
+        showGameOver();
     }
+
+    public void fireSwipeEvent(SwipeEvent event){
+        event = evalSwipe(event);
+        for(SwipeListener listener: SwipeListeners){
+            listener.onSwipeEvent(event);
+        }
+        //implement score update here
+        //call method that updates score based on the event swipe
+        updateScore(event);
+    }
+
 
 }
