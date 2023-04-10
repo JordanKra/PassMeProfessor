cf1300f086cd2348dcf91d56dd6f3ac9a57c979a
zcythan <jferrick@umich.edu>
2023-04-10 18:29:51 -0400

    Cherry picked stuff

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -56,6 +56,7 @@ class MainActivity : AppCompatActivity() {
         blurredBackground.setImageBitmap(originalBitmap)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the screen to main screen
