01c4e36d1e5a6c39454469c701b4519805590330
zcythan <jferrick@umich.edu>
2023-04-10 18:11:21 -0400

    xc vcxfd

--- app/src/main/java/com/example/passmeprofessor/MainActivity.kt
+++ app/src/main/java/com/example/passmeprofessor/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.example.passmeprofessor
 
-import android.R.attr.button
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.os.Bundle
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -14,39 +10,8 @@ import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-import android.os.CountDownTimer
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var originalBitmap: Bitmap
-    private fun captureViewBitmap(view: View): Bitmap {
-        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        val bgDrawable = view.background
-        if (bgDrawable != null) {
-            bgDrawable.draw(canvas)
-        } else {
-            canvas.drawColor(android.graphics.Color.WHITE)
-        }
-        view.draw(canvas)
-        return bitmap
-    }
-
-    private fun applyBlurToBackground() {
-        val rootView = findViewById<View>(android.R.id.content)
-        val blurredBackground = findViewById<ImageView>(R.id.blurred_background)
-
-        originalBitmap = captureViewBitmap(rootView)
-        val mainActivityBitmap = captureViewBitmap(rootView)
-        val blurredBitmap = BlurBuilder.blur(this, mainActivityBitmap)
-        blurredBackground.setImageBitmap(blurredBitmap)
-    }
-
-    private fun removeBlurFromBackground() {
-        val blurredBackground = findViewById<ImageView>(R.id.blurred_background)
-        blurredBackground.setImageBitmap(originalBitmap)
-    }
-
 
     private lateinit var game: Game
 
@@ -62,30 +27,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Set the screen to main screen
         setContentView(R.layout.activity_main)
-        var blurred = true
-        val timerText = findViewById<TextView>(R.id.timerText)
-        //var timer =  Timer(95, timerText)
-
-        val blurButton = findViewById<Button>(R.id.blurButton)
-        blurButton.setOnClickListener {
-            blurred = if(blurred){
-                removeBlurFromBackground()
-                false
-            } else {
-                applyBlurToBackground()
-                true
-            }
-        }
-
-        val rootView = findViewById<View>(android.R.id.content)
-        rootView.post {
-            applyBlurToBackground()
-        }
-
-
-        var timer = Timer(95, timerText)
-        val paperSprite = Paper(findViewById<ImageView>(R.id.paper))
-        paperSprite.generateRandomPaper()
         paperView = findViewById(R.id.paper)
         //Create game instance
         game = Game()
